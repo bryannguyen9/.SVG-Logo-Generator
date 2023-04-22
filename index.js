bd125a7b0b065229904a33dd9df26388fd0b05c3
bryannguyen9 <127802656+bryannguyen9@users.noreply.github.com>
2023-04-21 21:14:02 -0700

    added shapes portion to user prompt using inquire method

--- index.js
+++ index.js
@@ -27,3 +27,20 @@ const questions = [
       message: answers => `Enter a color keyword or hexadecimal number for the ${answers.shape}:`
     }
   ];
+
+  inquirer.prompt(questions).then(answers => {
+    // Create shape object based on user's choice
+    let shape;
+    switch (answers.shape) {
+      case 'circle':
+        shape = new Circle(answers.shapeColor);
+        break;
+      case 'triangle':
+        shape = new Triangle(answers.shapeColor);
+        break;
+      case 'square':
+        shape = new Square(answers.shapeColor);
+        break;
+      default:
+        throw new Error('Invalid shape choice');
+    }});
