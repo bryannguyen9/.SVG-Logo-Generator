a08aefc075f2177b4a60b5997cf98ad0dcf1fc72
bryannguyen9 <127802656+bryannguyen9@users.noreply.github.com>
2023-04-22 14:16:17 -0700

    added triangle and square.js and test.js

--- index.js
+++ index.js
@@ -6,58 +6,60 @@ const Circle = require('./lib/circle.js');
 
   inquirer
     .prompt([
-        {
-        type: 'input',
-        name: 'text',
-        message: 'Enter up to three characters for the logo text:',
-        validate: input => input.length <= 3 || 'Please enter up to three characters'
-        },
-        {
-        type: 'input',
-        name: 'textColor',
-        message: 'Enter a color keyword or hexadecimal number for the text:',
-        validate: input => !isNaN(parseInt(input)) ? 'Color must be a color keyword or hexadecimal value ie) blue or #0000FF' : true
-        },
-        {
-        type: 'list',
-        name: 'shape',
-        message: 'Select a shape:',
-        choices: ['circle', 'triangle', 'square']
-        },
-        {
-        type: 'input',
-        name: 'shapeColor',
-        message: answers => `Enter a color keyword or hexadecimal number for the ${answers.shape}:`,
-        validate: input => !isNaN(parseInt(input)) ? 'Color must be a color keyword or hexadecimal value ie) blue or #0000FF' : true
-        }
+      {
+      type: 'input',
+      name: 'text',
+      message: 'Enter up to three characters for the logo text:',
+      validate: input => input.length <= 3 || 'Please enter up to three characters'
+      },
+      {
+      type: 'input',
+      name: 'textColor',
+      message: 'Enter a color keyword or hexadecimal number for the text:',
+      validate: input => !isNaN(parseInt(input)) ? 'Color must be a color keyword or hexadecimal value ie) blue or #0000FF' : true
+      },
+      {
+      type: 'list',
+      name: 'shape',
+      message: 'Select a shape:',
+      choices: ['circle', 'triangle', 'square']
+      },
+      {
+      type: 'input',
+      name: 'shapeColor',
+      message: answers => `Enter a color keyword or hexadecimal number for the ${answers.shape}:`,
+      validate: input => !isNaN(parseInt(input)) ? 'Color must be a color keyword or hexadecimal value ie) blue or #0000FF' : true
+      }
     ])
-    .then((answers)=>{
-        switch (answers.desired_shape[0]){
-          case 'Triangle':
-            const renderedTriangle = new Triangle(answers.text.trim(), answers.textColor.trim(), answers.shapeColor.trim()).render();
-            return renderedTriangle;
-          case 'Square':
-            const renderedSquare = new Square(answers.text.trim(), answers.textColor.trim(), answers.shapeColor.trim()).render();
-            return renderedSquare;
-          case 'Circle':
-            const renderedCircle = new Circle(answers.text.trim(), answers.textColor.trim(), answers.shapeColor.trim()).render();
-            return renderedCircle;
+    .then((answers) => {
+      let renderedLogo;
+        switch (answers.shape) {
+          case 'triangle':
+            renderedLogo = new Triangle(answers.text.trim(), answers.textColor.trim(), answers.shapeColor.trim()).render();
+            break;
+          case 'square':
+            renderedLogo = new Square(answers.text.trim(), answers.textColor.trim(), answers.shapeColor.trim()).render();
+            break;
+          case 'circle':
+            renderedLogo = new Circle(answers.text.trim(), answers.textColor.trim(), answers.shapeColor.trim()).render();
+            break;
           default:
             throw new Error('Please choose a shape');
-        };
+        }
+        return renderedLogo;
       })
     .then((renderedLogo)=>{
-        return new Promise((resolve, reject) => {
-            fs.writeFile('./examples/renderedShape.svg', renderedLogo, (err) => {
-                if (err) {
-                reject(err);
-                } else {
-                console.log('Your SVG has been generated! Please check ./examples/');
-                resolve();
-                }
-            });
-        });
+      return new Promise((resolve, reject) => {
+          fs.writeFile('./examples/renderedShape.svg', renderedLogo, (err) => {
+              if (err) {
+              reject(err);
+              } else {
+              console.log('Your SVG has been generated! Please check ./examples/');
+              resolve();
+              }
+          });
+      });
     })
     .catch((err)=>{
-        console.log(err)
+      console.log(err)
     });
