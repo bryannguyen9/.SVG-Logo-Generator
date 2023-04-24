ab9f55533548aad3ff9305ca5bb9d72827ca9953
bryannguyen9 <127802656+bryannguyen9@users.noreply.github.com>
2023-04-23 18:12:02 -0700

    adding function that invokes tests and test console log when calling node index.js and not having to call npm run test

--- index.js
+++ index.js
@@ -5,65 +5,78 @@ const Triangle = require('./lib/triangle.js');
 const Square = require('./lib/square.js');
 const Circle = require('./lib/circle.js');
 
-  inquirer
-    .prompt([
-      {
+inquirer
+  .prompt([
+    {
       type: 'input',
       name: 'text',
       message: 'Enter up to three characters for the logo text:',
       validate: input => input.length <= 3 || 'Please enter up to three characters'
-      },
-      {
+    },
+    {
       type: 'input',
       name: 'textColor',
       message: 'Enter a color keyword or hexadecimal number for the text:',
       validate: input => !isNaN(parseInt(input)) ? 'Color must be a color keyword or hexadecimal value ie) blue or #0000FF' : true
-      },
-      {
+    },
+    {
       type: 'list',
       name: 'shape',
       message: 'Select a shape:',
       choices: ['circle', 'triangle', 'square']
-      },
-      {
+    },
+    {
       type: 'input',
       name: 'shapeColor',
       message: answers => `Enter a color keyword or hexadecimal number for the ${answers.shape}:`,
       validate: input => !isNaN(parseInt(input)) ? 'Color must be a color keyword or hexadecimal value ie) blue or #0000FF' : true
+    }
+  ])
+  .then((answers) => {
+    let renderedLogo;
+      switch (answers.shape) {
+        case 'triangle':
+          renderedLogo = new Triangle(answers.text.trim(), answers.textColor.trim(), answers.shapeColor.trim());
+          renderedLogo.renderTriangle();
+          break;
+        case 'square':
+          renderedLogo = new Square(answers.text.trim(), answers.textColor.trim(), answers.shapeColor.trim());
+          renderedLogo.renderSquare();
+          break;
+        case 'circle':
+          renderedLogo = new Circle(answers.text.trim(), answers.textColor.trim(), answers.shapeColor.trim());
+          renderedLogo.renderCircle();
+          break;
+        default:
+          throw new Error('Please choose a shape');
       }
-    ])
-    .then((answers) => {
-      let renderedLogo;
-        switch (answers.shape) {
-          case 'triangle':
-            renderedLogo = new Triangle(answers.text.trim(), answers.textColor.trim(), answers.shapeColor.trim());
-            renderedLogo.renderTriangle();
-            break;
-          case 'square':
-            renderedLogo = new Square(answers.text.trim(), answers.textColor.trim(), answers.shapeColor.trim());
-            renderedLogo.renderSquare();
-            break;
-          case 'circle':
-            renderedLogo = new Circle(answers.text.trim(), answers.textColor.trim(), answers.shapeColor.trim());
-            renderedLogo.renderCircle();
-            break;
-          default:
-            throw new Error('Please choose a shape');
-        }
-        return renderedLogo;
-      })
-      .then((renderedLogo)=>{
-        return new Promise((resolve, reject) => {
-            fs.writeFile('./examples/logo.svg', renderedLogo.toString(), (err) => {
-                if (err) {
-                reject(err);
-                } else {
-                console.log('Your SVG has been generated! Please check ./examples/');
-                resolve();
-                }
-            });
+      return renderedLogo;
+  })
+  .then((renderedLogo)=>{
+    return new Promise((resolve, reject) => {
+        fs.writeFile('./examples/logo.svg', renderedLogo.toString(), (err) => {
+          const { exec } = require('child_process');
+          exec('npm run test', (error, stdout, stderr) => {
+            if (error) {
+              console.log(`error: \n${error.message}`);
+              return;
+            }
+            if (stderr) {
+              console.log(`Test Status: \n${stderr}`);
+              return;
+            }
+            console.log(`stdout: \n${stdout}`);
+          });
+
+          if (err) {
+            reject(err);
+          } else {
+            console.log('Your SVG has been generated! Please check ./examples/ folder\n');
+            resolve();
+          }
         });
-      })
+    });
+  })
     .catch((err)=>{
       console.log(err)
     });
